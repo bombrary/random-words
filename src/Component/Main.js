542dfbd4f131f3d6624ea583081622fd984eae16
bombrary <bombra108@gmail.com>
2022-12-21 10:30:21 +0900

    add preset words loading

--- src/Component/Main.js
+++
@@ -1,9 +0,0 @@
-exports.readBlobAsString = (blob) => () => {
-  console.log(blob)
-  const reader = new FileReader();
-  reader.readAsText(blob);
-  reader.onload = () => {
-    console.log(reader.result)
-  }
-  return reader.result;
-}
--- src/Component/Main.purs
+++ src/Component/Main.purs
@@ -4,15 +4,14 @@ module Component.Main where
 import Prelude
 
 import Affjax as AX
-import Affjax.ResponseFormat as AXRF
+import Affjax.ResponseFormat (string)
 import Choose.Distinct (choose)
 import Control.Monad.Except as Except
 import DOM.HTML.Indexed.InputAcceptType (InputAcceptType(..), InputAcceptTypeAtom(AcceptMediaType))
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Data.MediaType (MediaType(..))
-import Data.String.Utils (lines, toCharArray)
-import Effect (Effect)
+import Data.String.Utils (lines)
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
@@ -22,18 +21,21 @@ import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Query.Event as HQE
-import Util (split)
 import Web.File.Blob (Blob)
-import Web.File.Blob as Blob
 import Web.File.File as File
-import Web.File.FileReader (FileReader)
 import Web.File.FileReader as FileReader
 import Web.HTML.Event.EventTypes as ET
 
+type BasicWords = { nouns :: Array String
+                  , adjectives :: Array String
+                  , adverbs :: Array String
+                  , verbs :: Array String
+                  }
 
 type State =
   { words :: Maybe (Array String)
   , randomWords :: Maybe (Array String)
+  , basicWords :: Maybe BasicWords
   }
 
 
@@ -41,6 +43,8 @@ data Action
   = Randomize
   | FileSelected Blob
   | FileLoaded H.SubscriptionId FileReader.FileReader 
+  | Initialize
+  | SetWords String
   | NoAction
 
 
@@ -51,6 +55,7 @@ component = H.mkComponent
   , render
   , eval: H.mkEval $ H.defaultEval
             { handleAction = handleAction
+            , initialize = Just Initialize
             }
   }
 
@@ -59,6 +64,7 @@ initialState :: forall input. input -> State
 initialState _ =
   { words: Nothing
   , randomWords: Nothing
+  , basicWords: Nothing
   }
 
 
@@ -70,17 +76,33 @@ render { randomWords } =
         [ HP.classes [ HH.ClassName "logo" ]
         , HP.src "logo.svg"
         ]
-        --map
-        --  (\c -> HH.span_ [ HH.text c ])
-        --  (toCharArray "Random Words")
+    , HH.div
+        [ HP.classes [ HH.ClassName "description" ]
+        ]
+        [ HH.p_ [ HH.text "生成ボタンを押すと、ランダムに単語が生成されます。" ]
+        , HH.p_ [ HH.text "生成する単語の品詞は、生成ボタン右の選択メニューから選べます。またそれ以外にも、「ファイルを選択」ボタンから自分の単語データを読み込むことができます。ただし単語データのファイルは、単語が改行で区切られたテキストファイルとします。"]
+        , HH.p_ [ HH.text "品詞ごとの単語データは"
+                , HH.a [ HP.href "https://bond-lab.github.io/wnja/" ] [ HH.text "日本語Wordnet" ]
+                , HH.text "より抽出しました。"
+                ]
+        ]
     , HH.div
         [ HP.classes [ HH.ClassName "random-words" ]] $
-        [ renderRandomWordsTable randomWords ]
+        [ renderRandomWords randomWords ]
     , HH.button
         [ HE.onClick (\_ -> Randomize)
         , HP.classes [ HH.ClassName "randomize-btn" ]
         ]
-        [ HH.text "ランダムをもう一度。"
+        [ HH.text "単語を生成"
+        ]
+    , HH.select
+        [ HE.onValueChange SetWords
+        ]
+        [ HH.option [ HP.value "nouns" ] [ HH.text "名詞" ]
+        , HH.option [ HP.value "verbs" ] [ HH.text "動詞" ]
+        , HH.option [ HP.value "adjectives" ] [ HH.text "形容詞" ]
+        , HH.option [ HP.value "adverbs" ] [ HH.text "副詞" ]
+        -- , HH.option [ HP.value "all" ] [ HH.text "全て" ]
         ]
     , HH.input
         [ HP.type_ HP.InputFile
@@ -96,29 +118,51 @@ render { randomWords } =
     ]
     
 
-renderRandomWordsTable :: forall w i. Maybe (Array String) -> HH.HTML w i
-renderRandomWordsTable randomWords =
+renderRandomWords :: forall w i. Maybe (Array String) -> HH.HTML w i
+renderRandomWords randomWords =
   case randomWords of
     Nothing ->
       HH.div_ []
 
     Just words ->
-      HH.table_ $
+      HH.ul_ $
         map
-          (\rows ->
-              HH.tr_ $
-                map
-                  (\word ->
-                      HH.td_
-                        [ HH.text word ]
-                  )
-                  rows
-
+          (\word ->
+              HH.li_ [ HH.text word ]
           )
-          (split 5 words)
+          words
+
+
+
+getBasicWords :: forall m. MonadAff m => m (Either AX.Error BasicWords)
+getBasicWords = do
+  adjectives <- liftAff $ AX.get string "/data/adjectives.txt"
+  nouns <- liftAff $ AX.get string "/data/nouns.txt"
+  verbs <- liftAff $ AX.get string "/data/verbs.txt"
+  adverbs <- liftAff $ AX.get string "/data/adverbs.txt"
+  pure $ (\adj n v adv -> { adjectives : lines adj.body
+                          , nouns : lines n.body
+                          , verbs : lines v.body
+                          , adverbs : lines adv.body })
+           <$> adjectives
+           <*> nouns
+           <*> verbs
+           <*> adverbs
+
 
 handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
 handleAction = case _ of
+  Initialize -> do
+    result <- liftAff getBasicWords
+    case result of
+      Left _ ->
+        pure unit
+
+      Right basicWords ->
+        H.modify_ _{ basicWords = Just basicWords
+                   , words = Just basicWords.nouns
+                   }
+
   Randomize -> do
      { words } <- H.get
      case words of
@@ -139,7 +183,7 @@ handleAction = case _ of
        HQE.eventListener
          ET.load
          (FileReader.toEventTarget reader)
-         (\e -> Just $ FileLoaded sid reader)
+         (\_ -> Just $ FileLoaded sid reader)
         
      pure unit
 
@@ -153,5 +197,29 @@ handleAction = case _ of
       Right word -> do
         H.modify_ _ { words = Just $ lines word }
         handleAction Randomize
+
+
+  SetWords value -> do
+     case value of
+       "nouns" ->
+         H.modify_ $ \s -> s { words = (_.nouns) <$> s.basicWords  }
+
+       "verbs" -> 
+         H.modify_ $ \s -> s { words = (_.verbs) <$> s.basicWords  }
+
+       "adjectives" ->
+         H.modify_ $ \s -> s { words = (_.adjectives) <$> s.basicWords  }
+
+       "adverbs" ->
+         H.modify_ $ \s -> s { words = (_.adverbs) <$> s.basicWords  }
+
+       "all" -> 
+         pure unit
+
+       "" ->
+         pure unit
+
+       str -> liftEffect $ log ("No value: " <> str)
+      
 
   NoAction -> pure unit
